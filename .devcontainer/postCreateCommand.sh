b588aa44f257547c9b7e6426ea964fd99ca9977d
naritata <narita.takeshi@optimind.tech>
2024-12-01 11:34:58 +0900

    build: devcontainer等の開発環境を整備

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+## コンテナで使いたいaliasを設定する
+cp .devcontainer/.bash_aliases ~/.bash_aliases
+
+## ホスト側で使っていて、コンテナでも使いたいsshの設定をコピーする
+## .devcontainer/.ssh/配下に予めconfigや秘密鍵を配置しておくこと
+cp .devcontainer/.ssh/* ~/.ssh/
+
+## python関連パッケージのインストール
+uv sync
